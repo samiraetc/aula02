646502c339c33813968d80fb4ff7b46090cb95b1
Samira Costa <samiracosta@aquaporservicos.pt>
2024-02-08 00:17:20 +0000

    criei os arquivos html e js

---
+++ scripts/index.js
@@ -0,0 +1,68 @@
+// let nome = "Samira"
+// let idade = 28
+// let altura = 1.58
+// let precoBanana = 1.99
+// let professora = true
+// let aluno = false
+
+// const sobrenome = "Costa"
+// // console.log(nome)
+// // console.log(sobrenome)
+// nome = 'Luana'
+
+
+// const numero1 = 10
+// const numero2 = 34
+
+// // console.log('soma', numero1 + numero2, 'batata')
+// // console.log('subtracao', numero1 - numero2)
+// // console.log('multipliccao', numero1 * numero2)
+// // console.log('divisao', numero1 / numero2)
+
+
+// let valorProduto = 56.87
+// let desconto = 0.7
+
+// // console.log('valor com desconto', valorProduto - (valorProduto * desconto))
+
+
+// let textoA = 'Samira'
+// let textoB = 'Costa'
+// let idadeX = 26
+
+
+// let nomeCompleto = textoA + textoB
+
+// console.log(nomeCompleto)
+
+
+// const nomeCompleto2 = `${textoA} ${textoB}` // concatenar utilizando craze
+
+// console.log(nomeCompleto2)
+
+
+// let t = '1'
+// let x = 2
+// let z = 3
+
+
+
+// console.log(nome + ' ' + sobrenome)
+
+
+
+const nome = 'Samira'
+const sobrenome = 'Costa'
+const anoAtual = 2024
+const idade = 22
+
+
+console.log(nome + ' ' + sobrenome)
+console.log(`${nome} ${sobrenome}`)
+
+
+
+let valor1 = parseInt(prompt('Digite um numero: '))
+
+console.log(valor1)
+
